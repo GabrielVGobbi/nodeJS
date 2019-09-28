20ec9cf705cfe310a2e25e0f16cddf0893263d16
gabriel gobbi <gabriel.gobbi15@gmail.com>
2019-09-28 12:40:08 -0300

    ok

--- src/routes.js
+++ src/routes.js
@@ -7,11 +7,17 @@ const express = require('express');
 const routes = express.Router();
 
 const ProductController = require('./controllers/ProductController');
-
 routes.get('/products', ProductController.index );
 routes.get('/products/:id', ProductController.show );
 routes.post('/products/add', ProductController.store );
 routes.put('/products/edit/:id', ProductController.update );
 routes.delete('/products/delete/:id', ProductController.destroy );
+
+const UserController = require('./controllers/UserController');
+routes.get('/user', UserController.index );
+routes.get('/user/:id', UserController.show );
+routes.post('/user/add', UserController.store );
+routes.put('/user/edit/:id', UserController.update );
+routes.delete('/user/delete/:id', UserController.destroy );
 
 module.exports = routes;
